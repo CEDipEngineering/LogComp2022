a10ee05e044ca4d5279550b2c4daad922a73a717
CEDipEngineering <cadudip@hotmail.com>
2022-02-15 13:03:28 -0300

    Finished version v0.1.0

--- main.py
+++ main.py
@@ -18,13 +18,58 @@ class Compiler:
         return input
     
     def semantics(self, input: string)->string:
-        
-        p = [m.start() for m in re.finditer('+', input) if '+' in input] 
-        m = [m.start() for m in re.finditer('-', input) if '+' in input]
-        calc = sorted(p+m)    
-        print(calc)
-        
-        
+        symbols = ['+', '-']
+        # match_dict = {}
+        # i = 0
+        # for char in input:
+        #     if char in symbols:
+        #         match_dict[i] = char
+        #     i+=1
+        # n=len(match_dict.keys())
+        # j = 0
+        # while j<n-1:
+        #     print(input[match_dict[j]:match_dict[j+1]])
+        #     j+=1
+        # print(match_dict) 
+        # 
+        running = True
+        while running:
+            try:
+                if len(input) == 0 or ('+' not in input and '-' not in input):
+                    running = False
+                    continue
+                curr_op = None
+                a_str = ''
+                b_str = ''
+                a = 0
+                b = 0
+                i=0
+                while input[i] not in symbols:
+                    i+=1
+                a_str = input[0:i]
+                j = i+1
+                while j<len(input) and input[j] not in symbols:
+                    j+=1
+                b_str = input[i+1:j]
+                a_str = a_str.replace(" ", "")
+                b_str = b_str.replace(" ", "")
+                try:
+                    a = int(a_str)
+                    b = int(b_str)
+                except ValueError:
+                    print("Invalid string input, try again!")
+                    return ""
+                # print(a_str)
+                # print(input[i])
+                # print(b_str)
+                if input[i] == '+':
+                    input = input.replace(input[0:j], str(a + b),1)
+                elif input[i] == '-':   
+                    input = input.replace(input[0:j], str(a - b),1)
+            except IndexError as e:
+                print(e)
+                print(i, j, input)
+                break
         return input
 
     def main(self, input: string):
@@ -37,11 +82,10 @@ class Compiler:
 
 if __name__ == "__main__":
     compiler = Compiler()
-    print(compiler.main('1+1+2+3'))
     if len(sys.argv) < 2:
         print("Send more args plz")
         exit(1)
-    print("Compiling...")
-    #print(compiler.main(sys.argv[1]))
+    # print("Compiling...")
+    print(compiler.main(sys.argv[1]))
     exit(0)
     
