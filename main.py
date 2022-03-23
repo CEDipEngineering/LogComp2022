aa8b20ac01e87de073e8018da364592a273f4ff9
CEDipEngineering <cadudip@hotmail.com>
2022-03-23 16:33:53 -0300

    Changed to AST

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import string
 import sys
 import re
+from typing import List
 
 class Token():
     def __init__(self, type, value):
@@ -77,62 +78,101 @@ class Tokenizer():
             print(curr_token)
             curr_token = self._advance()
 
+class Node:
+    def __init__(self, value, children: List['Node']):
+        self.value = value
+        self.children = children
+
+    def evaluate(self):
+        pass
+
+    def __str__(self):
+        return f'Node({self.value})=>[{[str(n) for n in self.children]}]'
+
+    def __repr__(self):
+        return self.__str__()
+
+class BinOp(Node):
+    def evaluate(self):
+        if self.value == '+':
+            return self.children[0].evaluate() + self.children[1].evaluate()
+        elif self.value == '*':
+            return self.children[0].evaluate() * self.children[1].evaluate()
+        elif self.value == '-':
+            return self.children[0].evaluate() - self.children[1].evaluate()
+        elif self.value == '/':
+            return self.children[0].evaluate() // self.children[1].evaluate()
+
+class UnOp(Node):
+    def evaluate(self):
+        if self.value == '+':
+            return self.children[0].evaluate()
+        elif self.value == '-':
+            return -self.children[0].evaluate()
+
+class IntVal(Node):
+    def evaluate(self):
+        return self.value
+
+class NoOp(Node):
+    def evaluate(self):
+        pass
+
 class Parser():
     tokens: Tokenizer
 
     def parseExpression():
         # print(Parser.tokens.actual , " EXPRESSION")
-        result = Parser.parseTerm()
+        node = Parser.parseTerm()
         operation = Parser.tokens.actual
         while operation.type == "PLUS" or operation.type == "MINUS":
             Parser.tokens.selectNext()
             if operation.type == "PLUS":
-                result = result + Parser.parseTerm()
+                node = BinOp(value='+', children=[node, Parser.parseTerm()])
             elif operation.type == "MINUS":
-                result = result - Parser.parseTerm()
+                node = BinOp(value='-', children=[node, Parser.parseTerm()])
             operation = Parser.tokens.actual
             if operation.type == "INT":
                 raise SyntaxError("Expected valid operation, instead got integer")
-        return result
+        return node
 
     def parseTerm():
-        # print(Parser.tokens.actual , " TERM")
-        result = Parser.parseFactor()
+        node = Parser.parseFactor()
         operation = Parser.tokens.actual
         if operation.type == "INT":
             raise SyntaxError("Expected valid operation, instead got integer")
         while operation.type == "MULT" or operation.type == "DIV":
             Parser.tokens.selectNext()
             if operation.type == "MULT":
-                result = result * Parser.parseFactor()
+                node = BinOp(value='*', children=[node, Parser.parseFactor()])
                 operation = Parser.tokens.actual
             elif operation.type == "DIV":
-                result = result // Parser.parseFactor()
+                node = BinOp(value='/', children=[node, Parser.parseFactor()])
                 operation = Parser.tokens.actual
             # operation = Parser.tokens.selectNext()
             # if operation.type == "INT":
             #     raise SyntaxError("Expected valid operation, instead got integer")
-        # print(f"Term {result=}")
-        return result
+        # print(f"Term {node=}")
+        return node
 
     def parseFactor():
         # print(Parser.tokens.actual , " FACTOR")
         operation = Parser.tokens.actual
         if operation.type == "INT":
             Parser.tokens.selectNext()
-            return operation.value
+            return IntVal(value=operation.value, children=[])
         elif operation.type == "PLUS":
             Parser.tokens.selectNext()
-            return Parser.parseFactor()
+            return UnOp(value='+', children=Parser.parseFactor())
         elif operation.type == "MINUS":
             Parser.tokens.selectNext()
-            return -Parser.parseFactor()
+            return UnOp(value='-', children=Parser.parseFactor())
         elif operation.type == "OP":
             Parser.tokens.selectNext()
-            result = Parser.parseExpression()
+            node = Parser.parseExpression()
             if Parser.tokens.actual.type == "CP":
                 Parser.tokens.selectNext()
-                return result
+                return node
             else:
                 raise SyntaxError("Failed to close parentheses")
         else:
@@ -158,7 +198,9 @@ def main(argv: list, argc: int):
     _ = Prepro()
 
     # Parser.debug_run(Prepro.filter(argv[1]))
-    print(Parser.run(Prepro.filter(argv[1])))
+    root = Parser.run(Prepro.filter(argv[1]))
+    # print(root)
+    print(root.evaluate())
     return 0
 
 if __name__ == "__main__":
